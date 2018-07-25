26b2758f34ad72c5e3f86367c7231fe32878ed69
Anion <littlesalo3@gmail.com>
2018-07-25 16:54:35 +0300

    second commit

--- src/Components/ExemplarListComponent.js
+++ src/Components/ExemplarListComponent.js
@@ -2,20 +2,16 @@ import React     from 'react'
 import Exemplar  from './exemplar'
 import './styles.css'
 
-
 const helperRenderList = ({ListForExemplars}) => {
-
-	const value = ListForExemplars.map(component => <div key = {component.id}><Exemplar component = {component}/> </div>);
-	console.table(value);
-	return( 
-		<div className = "example"> 
-
-			{value}
-
-		</div>
+	const value = ListForExemplars.map(
+		component => <div key = {component.id}><Exemplar component = {component}/> </div>
 	)
 
-} 
+	return(
+		<div className = "example">
+			{value}
+		</div>
+	)
+}
 
-export default helperRenderList;
-
+export default helperRenderList
--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-import React            from 'react';
+import React            from 'react'
 import {render}         from 'react-dom'
 import Exemplars        from './Components/ExemplarListComponent'
 import ListForExemplars from './Components/listForExemplars'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const App = () => {
-
 	return (
-		
-		<div className = "col-md-4 card">
-
+		<div className="col-md-12 card">
 			<Exemplars ListForExemplars = {ListForExemplars} />
-
 		</div>
-
 	)
-
 }
-
 
 render(<App/>, document.getElementById("root"))
